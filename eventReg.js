d1f4184cfa6e489f3fc4a48fc63ca94e0e9a9e13
Bryan Yunis <bbyunis@gmail.com>
2018-10-10 12:58:54 -0600

    adding all articles to db

--- eventReg.js
+++ eventReg.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://bry:weioWEIO8*@cluster0-ovni5.mongodb.net/project3')
+mongoose.connect('mongodb+srv://bry:weioWEIO8*@cluster0-ovni5.mongodb.net/project3?retryWrites=true', { useNewUrlParser: true })
 const Article = require('./models/article')
 
 const {
@@ -31,7 +31,6 @@ const returnInfo = new ReturnInfo({
   locationInfo: locationInfoFlags
 });
 
-const recentQ = new GetRecentArticles(er, { maxCount: 2000 });
 
 async function fetchfilteredUpdates() {
   const query = new QueryArticles({
@@ -42,59 +41,52 @@ async function fetchfilteredUpdates() {
   query.setRequestedResult(
     new RequestArticlesRecentActivity({
       // download at most 2000 articles. if less of matching articles were added in last 10 minutes, less will be returned
-      maxArticleCount: 10,
+      maxArticleCount: 1000,
       // consider articles that were published at most 10 minutes ago
       updatesAfterMinsAgo: 10000,
       returnInfo: returnInfo,
       mandatorySourceLocation: true
     })
   );
-  
+
   const articleList = await er.execQuery(query, returnInfo);
 
-  let array = [];
   var count = 0;
 
   //set up database connection
-  const savedArticle = new Article()
 
 
-  for (const article of lodash.get(articleList, "recentActivityArticles.activity",[])) {
+  for (const article of lodash.get(articleList, "recentActivityArticles.activity", [])) {
     if (article.location) {
-    savedArticle.title = JSON.stringify(article.title)
-    savedArticle.body = JSON.stringify(article.body)
-    savedArticle.date = JSON.stringify(article.date)
-    savedArticle.location = JSON.stringify(article.location)
-    savedArticle.lat = parseInt(JSON.stringify(article.location.lat))
-    savedArticle.lng = parseInt(JSON.stringify(article.location.long))
-    
-    savedArticle.save(err => {
-      if (err) console.log(err)
-    })
+      console.log('++++++++++NEW ARTICLE+++++++++++++')
+      const savedArticle = new Article()
 
-    array.push(JSON.stringify(article));
-    // console.info('whole article', article)
-    count++;
-    //     const string = JSON.stringify(article)
+      savedArticle.title = JSON.stringify(article.title)
+      savedArticle.body = JSON.stringify(article.body)
+      savedArticle.date = JSON.stringify(article.date)
+      savedArticle.location = JSON.stringify(article.location.label.eng)
+      savedArticle.lat = parseInt(JSON.stringify(article.location.lat))
+      savedArticle.lng = parseInt(JSON.stringify(article.location.long))
 
-    // console.info(`latitude ${JSON.stringify(article.location.lat, null, 2)}
+      // articleObj = {
+      //   title: JSON.stringify(article.title),
+      //   body: JSON.stringify(article.body),
+      //   date: JSON.stringify(article.date),
+      //   location: JSON.stringify(article.location),
+      //   lat: parseInt(JSON.stringify(article.location.lat)),
+      //   lng: parseInt(JSON.stringify(article.location.long))
+      // }
+
+      savedArticle.save()
+      // console.info('whole article', article)
+      count++;
+      //     const string = JSON.stringify(article)
+
+      // console.info(`latitude ${JSON.stringify(article.location.lat, null, 2)}
+    }
   }
-  }
-  // fs.appendFile("./1009-2323.json", array, err => {
-  //   if (err) throw err;
-  //   console.log(count);
-  // });
+  console.log('count', count)
   // wait exactly a minute until next batch of new content is ready
   await sleep(60 * 1000);
 }
 fetchfilteredUpdates();
-
-async function getRecentUpdates(recentQ) {
-  const articleList = await recentQ.getUpdates();
-  console.log(articleList);
-
-  await sleep(60 * 1000);
-  await getRecentUpdates(recentQ);
-}
-
-getRecentUpdates(recentQ);
